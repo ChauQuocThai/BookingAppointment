4691705961e8f25aefc0e2c33f9fd77dbe157f08
NT NGUYÊN <ntnguyen10222002@gmail.com>
2023-09-10 10:03:27 +0700

    Create All Models, Migrations and Seeders

---
+++ src/migrations/migration-create-specialty.js
@@ -0,0 +1,38 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        // currentNumber: DataTypes.INTEGER,
+        // maxNumber: DataTypes.INTEGER,
+        // date: DataTypes.DATE,
+        // timeType: DataTypes.STRING,
+        // doctorId: DataTypes.INTEGER,
+        await queryInterface.createTable('specialties', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            description: {
+                type: Sequelize.TEXT
+            },
+            image: {
+                type: Sequelize.STRING
+            },
+
+
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        });
+    },
+    async down(queryInterface, Sequelize) {
+        await queryInterface.dropTable('specialties');
+    }
+};
